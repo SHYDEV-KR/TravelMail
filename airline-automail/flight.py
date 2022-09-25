e370e5405267ef59766401f659bacd542964c1c8
SHYDEV <ekdmbzzang@naver.com>
2022-09-25 20:46:18 +0900

    Initial Commit

---
+++ airline-automail/flight.py
@@ -0,0 +1,58 @@
+import time # sleep 활용을 위해
+from selenium import webdriver 
+from bs4 import BeautifulSoup
+
+import pandas as pd
+
+def arrangeFlightData(dataList: list):
+  df = list()
+  for d in dataList:
+    d[0].append(d[-1])
+    d[1].append("-")
+    df.append(d[0])
+    df.append(d[1])
+    df.append(["-"] * 6)
+  
+  df = pd.DataFrame(df, columns=['항공사', '출발', '소요시간', '도착', '종류', '총 가격'])
+  df_html = df.to_html(index=False, justify='center')
+  df_html = df_html.replace('<table border="1" class="dataframe">', '<table border="0" class="dataframe" bgcolor=black cellpadding=1 cellspacing=1><tr><td><table border="0" class="dataframe" bgcolor=black>')
+  df_html = df_html.replace('</table>', '</table> </td></tr></table>')
+  df_html = df_html.replace('<td>','<td bgcolor=white>')
+  df_html = df_html.replace('<th>','<th bgcolor=#e5e5e5>')
+  return df_html
+
+
+def getFlightData(url):
+  specific_options = webdriver.ChromeOptions()
+  specific_options.add_argument('--headless')
+  specific_options.add_argument('--no-sandbox')
+
+  driver = webdriver.Chrome('chromedriver', options = specific_options)
+  driver.set_window_size(1920, 1080)
+  driver.get(url)
+  time.sleep(3)
+  driver.save_screenshot('price.png')
+
+  result = []
+
+  soup = BeautifulSoup(driver.page_source, 'html.parser')
+  for i, e in enumerate(soup.find_all("div", class_="mrt_foreign_wrap k1_clearfix")):
+    result.append([])
+    result[i] = [string.text.strip("\n ") for string in e.find_all("div", class_="list")]
+    result[i].append(e.find("span", class_="fare_total").text)
+    result[i][0] = result[i][0].split("\n")
+    result[i][1] = result[i][1].split("\n")
+
+  result = result[1:]
+
+  for r in result:
+    for i in range(len(r)):
+      count = 0
+      for j in range(len(r[i])):
+        if '' == r[i][j]:
+          count += 1
+      
+      for _ in range(count):
+        r[i].remove('')
+
+  return arrangeFlightData(result)
---
+++ airline-automail/main.py
@@ -0,0 +1,77 @@
+import flight
+import smtplib
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+from email.mime.application import MIMEApplication
+import datetime as dt
+import private
+privateKeys = private.myKeys()
+
+def formatDateToKorean():
+  today = dt.datetime.now()
+  return f"{today.month}월 {today.day}일"
+
+recipients = privateKeys["recipients"]
+
+title = f"[{formatDateToKorean()}] 후쿠오카 왕복 최저가"
+
+message = MIMEMultipart()
+message['Subject'] = title
+message['From'] = privateKeys["sender_email"]
+message['To'] = ",".join(recipients)
+
+filename = "price.png"
+fp = open(filename, 'rb')
+att = MIMEApplication(fp.read(), _subtype="pdf")
+fp.close()
+att.add_header('Content-Disposition', 'attachment', filename=filename)
+message.attach(att)
+
+flightTable = flight.getFlightData(privateKeys["url"])
+buttonStyle = """
+  margin: 1rem 0 0 0;
+  padding: 0.5rem 1rem;
+  font-family: 'Noto Sans KR', sans-serif;
+  font-size: 0.75rem;
+  font-weight: 400;
+  text-align: center;
+  text-decoration: none;
+  display: inline-block;
+  width: auto;
+  color: white;
+  background-color: #B1B2FF;
+
+  border: none;
+  border-radius: 4px;
+  box-shadow: 0 4px 6px -1px rgba(0, 0, 0, 0.1), 0 2px 4px -1px rgba(0, 0, 0, 0.06);
+  cursor: pointer;
+  transition: 0.5s;
+"""
+
+content = f"""
+    <html>
+      <body>
+          <h3>{title}</h3>
+          <p>마음을 담아 보내드립니다 *^^*</p>
+          <p>사진도 참고하세요 ㅎㅎ</p>
+          {flightTable}
+          <p>위 표는 {dt.datetime.now()} 기준이며 몇 초 정도의 오차는 있을 수 있음</p>
+          <a href={privateKeys["url"]} style="{buttonStyle}">지금 바로 보러가기&rarr;</a>
+      </body>
+    </html>
+"""
+
+mimetext = MIMEText(content,'html')
+message.attach(mimetext)
+
+email_id = privateKeys["id"]
+email_pw = privateKeys["password"]
+
+server = smtplib.SMTP('smtp.naver.com',587)
+server.ehlo()
+server.starttls()
+server.login(email_id,email_pw)
+server.sendmail(message['From'],recipients,message.as_string())
+digit = "person" if len(recipients) == 1 else "people"
+print(f"✅ Mail sent to {len(recipients)} {digit}!")
+server.quit()
