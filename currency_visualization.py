521614995c9d5386be8493b12f56b46f870b62db
paulyoon28 <ekdmbzzang@naver.com>
2022-09-27 11:21:36 +0900

    generate image funtion add

--- currency_visualization.py
+++ currency_visualization.py
@@ -2,17 +2,17 @@ import pandas as pd
 import os
 import matplotlib.pyplot as plt
 
-df = pd.read_csv(os.path.dirname(os.path.realpath(__file__)) + "/" + "jpy_krw.csv", engine="python")
-df_recent_currency = df.iloc[-24:, -1]
+def generate_image():
+  df = pd.read_csv(os.path.dirname(os.path.realpath(__file__)) + "/" + "jpy_krw.csv", engine="python")
+  df_recent_currency = df.iloc[-24:, -1]
 
+  plt.figure(figsize=(10, 5))
 
-plt.figure(figsize=(10, 5))
+  df_recent_currency.plot()
+  plt.gca().invert_yaxis()
 
-df_recent_currency.plot()
-plt.gca().invert_yaxis()
+  plt.title("recent 24 JPY/KRW currency")
+  plt.xlabel("investigation index")
+  plt.ylabel("KRW")
 
-plt.title("recent 24 JPY/KRW currency")
-plt.xlabel("investigation index")
-plt.ylabel("KRW")
-
-plt.savefig(os.path.dirname(os.path.realpath(__file__)) + "/" + "recent_currency.png")
+  plt.savefig(os.path.dirname(os.path.realpath(__file__)) + "/" + "recent_currency.png")
