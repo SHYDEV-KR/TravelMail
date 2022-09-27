49f6cd860070c3bd84d07746958402ce03365de5
paulyoon28 <ekdmbzzang@naver.com>
2022-09-27 11:20:07 +0900

    feat currency visualization

---
+++ currency_visualization.py
@@ -0,0 +1,18 @@
+import pandas as pd
+import os
+import matplotlib.pyplot as plt
+
+df = pd.read_csv(os.path.dirname(os.path.realpath(__file__)) + "/" + "jpy_krw.csv", engine="python")
+df_recent_currency = df.iloc[-24:, -1]
+
+
+plt.figure(figsize=(10, 5))
+
+df_recent_currency.plot()
+plt.gca().invert_yaxis()
+
+plt.title("recent 24 JPY/KRW currency")
+plt.xlabel("investigation index")
+plt.ylabel("KRW")
+
+plt.savefig(os.path.dirname(os.path.realpath(__file__)) + "/" + "recent_currency.png")
